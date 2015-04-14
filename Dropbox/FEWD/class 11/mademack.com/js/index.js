85036e4d6607a961727952cb19a5e814015aad14
mackclark311 <mackclark311@gmail.com>
2015-04-13 20:54:15 -0400

    committing some stuff woooo

--- Dropbox/FEWD/class 11/mademack.com/js/index.js
+++ Dropbox/FEWD/class 11/mademack.com/js/index.js
@@ -12,9 +12,3 @@ $(window).scroll(function() {
  });
 
 
-if($.device  == (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()))){
-	$('#carrot').hide();
-	$('#slide-1').addClass('mobile-slide-1');
-	$('#slide-2').addClass('mobile-slide-2');
-	$('#slide-3').addClass('mobile-slide-3 ');
-}
